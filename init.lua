43df9808eac7d3fb53d02ecd6e1ddf5e3e1bed72
A.C.M <undertakers_help@yahoo.com>
2018-10-21 13:50:08 +0200

    Recipes fruitecoleslaw, rice-grapes-plate, tofufruitcreme added.

--- init.lua
+++ init.lua
@@ -55,7 +55,11 @@ if minetest.get_modpath("food") then
                         {"bees:bottle_honey","dye:red","bees:bottle_honey"},
                         {"group:food_sugar","farming:flour","cucina_vegana:sunflower_seeds"},
                         {"dye:green","group:food_egg","food_extend:baking_powder"}
-                    }
+                      },
+            replacements = {
+                            {"bees:bottle_honey", "vessels:glass_bottle 2"}
+                           }
+
         })
     end)
 
@@ -75,6 +79,79 @@ if minetest.get_modpath("food") then
         output = "food_extend:gingerbread_sam",
         recipe = "food_extend:gingerbread_sam_raw"
     })
+
+    food.module("tofufruitcreme",function()
+        minetest.register_craftitem("food_extend:tofufruitcreme", {
+            description = S("Tofu-Fruit-Creme"),
+            inventory_image = "food_extend_tofufruitcreme.png",
+            on_use = food.item_eat(3),
+            groups = {food = 1}
+        })
+        food.craft({
+            output = "food_extend:tofufruitcreme",
+            recipe = {
+                        {"farming_plus:banana","cucina_vegana:tofu","default:apple"},
+                        {"","group:food_bowl",""}
+                    }
+        })
+    end)
+
+    food.module("rice_grapes_plate_raw",function()
+        minetest.register_craftitem("food_extend:rice_grapes_plate_raw", {
+            description = S("Rice-Grapes-Plate (raw)"),
+            inventory_image = "food_extend_rice_grapes_plate_raw.png",
+            --on_use = food.item_eat(6),
+            groups = {food = 1}
+        })
+        food.craft({
+            output = "food_extend:rice_grapes_plate_raw",
+            recipe = {
+                        {"bees:bottle_honey","farming:grapes"},
+                        {"group:food_salt","cucina_vegana:bowl_rice"},
+                        {"group:food_meat","cucina_vegana:plate"}
+                    },
+            replacements = {
+                            {"bees:bottle_honey", "vessels:glass_bottle 2"},
+                            {"cucina_vegana:bowl_rice", "cucina_vegana:bowl"}
+                           }
+        })
+    end)
+
+    food.module("rice_grapes_plate",function()
+        minetest.register_craftitem("food_extend:rice_grapes_plate", {
+            description = S("Rice-Grapes-Plate"),
+            inventory_image = "food_extend_rice_grapes_plate.png",
+            on_use = food.item_eat(6),
+            groups = {food = 1}
+        })
+    end)
+
+    minetest.register_craft({
+        type = "cooking",
+        cooktime = 17,
+        output = "food_extend:rice_grapes_plate",
+        recipe = "food_extend:rice_grapes_plate_raw"
+    })
+
+    food.module("fruitcoleslaw",function()
+        minetest.register_craftitem("food_extend:fruitcoleslaw", {
+            description = S("Coleslaw with Fruits"),
+            inventory_image = "food_extend_fruitcoleslaw.png",
+            on_use = food.item_eat(5),
+            groups = {food = 1}
+        })
+        food.craft({
+            output = "food_extend:fruitcoleslaw",
+            recipe = {
+                        {"default:apple","cucina_vegana:kohlrabi"},
+                        {"group:food_orange","cucina_vegana:parsley"},
+                        {"cucina_vegana:soy_milk","group:food_bowl"}
+                    },
+            replacements = {
+                            {"cucina_vegana:soy_milk", "vessels:drinking_glass"}
+                           }
+        })
+    end)
     
     print("[MOD] " .. minetest.get_current_modname() .. "successfully loaded.")
     
